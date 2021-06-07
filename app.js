3b60f74fe01f6872be12013b7ae2ddf673e5caea
Shouvik Mandal <shouvik0013@gmail.com>
2021-06-07 21:41:05 +0530

    Two middleware added, zero response

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('First middleware');
+    next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('Second middleware');
+})
 
 const server = http.createServer(app);  // app is also a valid request handler
 
