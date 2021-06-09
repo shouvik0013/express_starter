4ca46d1326da35891f2b8fa82f4bcfccb7cd3e5f
Shouvik Mandal <shouvik0013@gmail.com>
2021-06-09 22:13:09 +0530

    body-parser is added

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs');
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.use('/add-product', (req, res, next) => {
-    console.log('Inside middleware');
-    res.send('<h2>Add product page</h2>');
+    // console.log('Inside middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 })
 
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
 // the single '/' tells the nodejs that the path starts with /
 // not the path only contains a '/'
 app.use('/', (req, res, next) => {
-    console.log('Second middleware');
+    // console.log('Second middleware');
     res.send('<h2>Response from the server</h2>')
 });
 
