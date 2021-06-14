0dd396e4a8b55d57b65e24bf9b7ab52eca09df03
Shouvik Mandal <shouvik0013@gmail.com>
2021-06-14 20:43:30 +0530

    post request handling mechanism added

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 })
 
-app.use('/product', (req, res, next) => {
+// this url is also executed for get, post 
+app.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
